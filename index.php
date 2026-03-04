b1ae554feba4216c73479e41048dcbdb4e9365b9
progamadorvinicius <vinicius.brow244@gmail.com>
2026-03-04 09:50:44 +0000

    boa

---
+++ index.php
@@ -0,0 +1,22 @@
+<!DOCTYPE html>
+<html lang="pt-br">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Aula 02</title>
+    </head>
+    <?php 
+    require_ once 'Caneta.php'; 
+    $c1 = new Caneta;
+    $c1->cor = "Azul";
+    $c10-> tampada = false; 
+    $c1-> tampar();
+    print_r($c1);
+    echo "<br/>";
+    $c2 = new Caneta; 
+    $c2->cor = "Verde";
+    $c2->carga = "50"; 
+    $c2->tampar();
+    print
+    ?>
+    </body>
